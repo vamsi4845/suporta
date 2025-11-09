74287890d141bdf46ffa53fd0088c62cfd836745
vamsisai <vamsisai.karumuri@gmail.com>
2025-11-09 17:34:42 +0530

    Update page.tsx

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -5,7 +5,6 @@ import { OrganizationSwitcher } from "@clerk/nextjs";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.addUser);
   return (
     <div className="flex items-center justify-center min-h-svh"> 
