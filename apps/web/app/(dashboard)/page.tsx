cb71afd934e39d5ba5468fab14a51867ab5969cd
vamsisai <vamsisai.karumuri@gmail.com>
2025-11-11 19:01:36 +0530

    fix: build warnings

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,19 +1,7 @@
-"use client";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "@workspace/backend/_generated/api";
-import { OrganizationSwitcher } from "@clerk/nextjs";
-import { Button } from "@workspace/ui/components/button";
-
 export default function Page() {
-  const addUser = useMutation(api.users.addUser);
   return (
     <div className="flex items-center justify-center min-h-svh"> 
-    <OrganizationSwitcher hidePersonal/>
-    <Button onClick={() => {
-      addUser({name: "John Doe"});
-    }}>
-      Add User
-    </Button>
+     Dashboard
     </div>
   )
 }
