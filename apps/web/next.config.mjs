24c13109b626890282d6b35c2af77bd9e36afd98
vamsisai <vamsisai.karumuri@gmail.com>
2025-11-12 16:46:55 +0530

    chore: remove sentry

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,23 +1,6 @@
-import {withSentryConfig} from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  experimental: {
-    serverExternalPackages: ["@sentry/nextjs"]
-  }
 }
 
-export default withSentryConfig(nextConfig, {
-  org: "robby-org",
-  project: "javascript-nextjs",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  tunnelRoute: "/monitoring",
-  disableLogger: true,
-  automaticVercelMonitors: true,
-  sourcemaps: {
-    assets: [".next/static/**"],
-    ignore: ["node_modules", "**/*_client-reference-manifest.js", "**/server/**"],
-    filesToDeleteAfterUpload: [".next/**/*.map"],
-  },
-});
+export default nextConfig;
